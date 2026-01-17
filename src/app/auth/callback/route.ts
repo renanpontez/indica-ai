fb76cb25a8a862fdf8611a43ab7aff6d1c497717
renan m <renanpontez@gmail.com>
2026-01-17 15:13:14 -0300

    fix: env vars

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -10,8 +10,8 @@ export async function GET(request: Request) {
   if (code) {
     const cookieStore = await cookies();
     const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      process.env.SUPABASE_URL!,
+      process.env.SUPABASE_KEY,
       {
         cookies: {
           getAll() {
