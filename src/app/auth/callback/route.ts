3a9eb377d32f6f32e75b0cce58d9e94b5f8f8ee9
renan m <renanpontez@gmail.com>
2026-01-17 15:14:55 -0300

    fix supabase key required

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -11,7 +11,7 @@ export async function GET(request: Request) {
     const cookieStore = await cookies();
     const supabase = createServerClient(
       process.env.SUPABASE_URL!,
-      process.env.SUPABASE_KEY,
+      process.env.SUPABASE_KEY!,
       {
         cookies: {
           getAll() {
